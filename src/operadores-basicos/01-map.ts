6c2e6ed70313b373440409cbfdfb253159a48013
SharonNavarro <shanmowrk@gmail.com>
2022-08-26 21:35:47 -0500

    o.b: 01-map

---
+++ src/operadores-basicos/01-map.ts
@@ -0,0 +1,15 @@
+import { range, fromEvent } from "rxjs";
+import { map } from "rxjs/operators";
+import { EnvironmentPlugin } from "webpack";
+
+// range(1,5).pipe(
+//     map<number, string>(val => (val * 10).toString())
+// )
+// .subscribe(console.log);
+
+const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
+const keyupCode$ = keyup$.pipe(
+    map(event => event.code)
+);
+
+keyupCode$.subscribe(code => console.log('map', code));
